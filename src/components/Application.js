4fc9fea92ed1c611ccf0f95751594c647c1db797
sarah-zhan <yizh.contact@gmail.com>
2023-03-21 20:45:23 -0700

    add daylist to nav bar

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React from 'react';
+import 'components/Application.scss';
+import DayList from './DayList';
 
-import "components/Application.scss";
+const days = [
+	{
+		id: 1,
+		name: 'Monday',
+		spots: 2,
+	},
+	{
+		id: 2,
+		name: 'Tuesday',
+		spots: 5,
+	},
+	{
+		id: 3,
+		name: 'Wednesday',
+		spots: 0,
+	},
+];
 
 export default function Application(props) {
-  return (
+	return (
 		<main className='layout'>
 			<section className='sidebar'>
 				<img
@@ -12,7 +30,13 @@ export default function Application(props) {
 					alt='Interview Scheduler'
 				/>
 				<hr className='sidebar__separator sidebar--centered' />
-				<nav className='sidebar__menu'></nav>
+				<nav className='sidebar__menu'>
+					<DayList
+						days={days}
+						day={'Monday'}
+						setDay={day => console.log(day)}
+					/>
+				</nav>
 				<img
 					className='sidebar__lhl sidebar--centered'
 					src='images/lhl.png'
