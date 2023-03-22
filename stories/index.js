f13c0547b1a4edcd4bf14d981ced42d6fd98a70e
sarah-zhan <yizh.contact@gmail.com>
2023-03-21 21:09:04 -0700

    render in daylist in application.js

--- stories/index.js
+++ stories/index.js
@@ -6,7 +6,7 @@ import { action } from '@storybook/addon-actions';
 import 'index.scss';
 
 import Button from 'components/Button';
-import DayListItem from '../src/components/DayListItem';
+import DayListItem from 'components/DayListItem';
 import DayList from 'components/DayList';
 
 storiesOf('Button', module)
