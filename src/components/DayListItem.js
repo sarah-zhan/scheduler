fde2c1c2627a02fbc65392f1749a4b9dc489cee5
sarah-zhan <yizh.contact@gmail.com>
2023-03-21 19:03:55 -0700

    style daylistitem

---
+++ src/components/DayListItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import classNames from 'classnames';
+import './DayListItem.scss';
+
+export default function DayListItem(props) {
+	const listClass = classNames('day-list__item', {
+		'day-list__item--selected': props.selected,
+		'day-list__item--full': props.spots === 0,
+	});
+	return (
+		<li onClick={() => props.setDay(props.name)} className={listClass}>
+			<h2 className='text--regular'>{props.name}</h2>
+			<h3 className='text--light'>{props.spots} spots remaining</h3>
+		</li>
+	);
+}
