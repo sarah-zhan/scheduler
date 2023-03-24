e2dce68d2a8a94f680f094244c739913ec71c46b
sarah-zhan <yizh.contact@gmail.com>
2023-03-23 19:20:36 -0700

    fetch days data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,25 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'components/Application.scss';
 import DayList from './DayList';
 import Appointment from './Appointment';
+import axios from 'axios';
 
-const days = [
-	{
-		id: 1,
-		name: 'Monday',
-		spots: 2,
-	},
-	{
-		id: 2,
-		name: 'Tuesday',
-		spots: 5,
-	},
-	{
-		id: 3,
-		name: 'Wednesday',
-		spots: 0,
-	},
-];
+
+
 
 const appointments = {
 	1: {
@@ -61,7 +47,18 @@ const appointments = {
 };
 
 export default function Application(props) {
-	const [day, setDay] = useState('Monday');
+	const [days, setDays] = useState([]);
+
+	useEffect(() => {
+		axios.get('/api/days')
+			.then((response) => {
+				setDays(response.data);
+			})
+			.catch((err) => {
+				console.log('error:', err.message);
+			});
+	}, []);
+
 	const schedule = Object.values(appointments).map(appointment => {
 		return (
 			<Appointment
@@ -83,7 +80,7 @@ export default function Application(props) {
 				/>
 				<hr className='sidebar__separator sidebar--centered' />
 				<nav className='sidebar__menu'>
-					<DayList days={days} value={day} onChange={setDay} />
+					<DayList days={days} value={props.day} onChange={props.setDay} />
 				</nav>
 				<img
 					className='sidebar__lhl sidebar--centered'
