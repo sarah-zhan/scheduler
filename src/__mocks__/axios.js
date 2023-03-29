f6ec3b001f5e3a73fdd8c0517e5ced568bb59383
sarah-zhan <yizh.contact@gmail.com>
2023-03-28 23:52:31 -0700

    test the put route

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -80,5 +80,31 @@ export default {
         data: fixtures.interviewers
       });
     }
-  })
+  }),
+
+  put: jest.fn(url => {
+    if (url === "/api/days") {
+      return Promise.resolve({
+        status: 204,
+        statusText: "No Content",
+        data: fixtures.days
+      });
+    }
+
+    if (url === "/api/appointments") {
+      return Promise.resolve({
+        status: 204,
+        statusText: "No Content",
+        data: fixtures.appointments
+      });
+    }
+
+    if (url === "/api/interviewers") {
+      return Promise.resolve({
+        status: 204,
+        statusText: "No Content",
+        data: fixtures.interviewers
+      });
+    }
+  }),
 };
