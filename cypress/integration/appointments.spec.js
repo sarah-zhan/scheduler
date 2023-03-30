d62d71f5d37cd9e676958df408db908bf8073c47
sarah-zhan <yizh.contact@gmail.com>
2023-03-29 20:53:20 -0700

    refactor test book an interview

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,11 +1,13 @@
 describe('Appointments', () => {
-  it('should book an interview', () => {
+  beforeEach(() => {
     cy.request('get', '/api/debug/reset');
 
     cy.visit("/");
 
     cy.contains('Monday');
+  });
 
+  it('should book an interview', () => {
     cy.get('[alt=Add]')
       .first()
       .click();
@@ -16,6 +18,10 @@ describe('Appointments', () => {
     cy.get("[alt='Sylvia Palmer']").click();
 
     cy.contains('Save').click();
+
+    cy.contains('.appointment__card--show', 'Lydia Miller-Jones');
+    cy.contains('.appointment__card--show', 'Sylvia Palmer');
+
 
   });
 
