7cdb44788a402cd2f91030ce596e8c876519a1f5
sarah-zhan <yizh.contact@gmail.com>
2023-03-30 21:45:23 -0700

    fix the error mode on save, back to edit mode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -5,16 +5,16 @@ function useVisualMode(initial) {
   const [history, setHistory] = useState([initial]);
   function transition(newMode, replace = false) {
     if (replace) {
-      setHistory(pre => [...pre.slice(0, pre.length - 1), newMode]);
+      setHistory(pre => [...pre.slice(0, pre.length - 1), newMode]); //if true, replace the previous mode
     } else {
-      setHistory(pre => [...pre, newMode]);
+      setHistory(pre => [...pre, newMode]); //if false, add new mode
     }
     setMode(newMode);
   }
   function back() {
     const pre = [...history];
     if (history.length > 1) {
-      pre.pop();
+      pre.pop(); //remove the last mode
     }
     setHistory(pre);
     setMode(pre[pre.length - 1]);
